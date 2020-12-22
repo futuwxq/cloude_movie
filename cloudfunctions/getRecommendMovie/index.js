0ec71a52ca81081f89631c4d8599aa0683328c73
Wangxq <3024881272@qq.com>
2020-12-22 19:55:13 +0800

    feat:add share_button

--- cloudfunctions/getRecommendMovie/index.js
+++ cloudfunctions/getRecommendMovie/index.js
@@ -7,11 +7,10 @@ const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
- return  db.collection('onemovie').get()
+return  db.collection('onemovie').get()
 //  db.collection('onemovie').get().then(res => {
 //     // res.data 包含该记录的数据
 //     console.log(res)
-//     return res.result.data
+//     return res
 //   })
-
 }
